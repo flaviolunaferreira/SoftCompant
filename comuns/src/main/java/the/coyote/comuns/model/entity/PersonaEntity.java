dc25e6970024bb2bafab40be097eb7ebf30b7722
theCoyote <flaviolunaferreira@gmail.com>
2024-06-26 16:01:17 -0300

    incluindo configurações de segurança

--- comuns/src/main/java/the/coyote/comuns/model/entity/PersonaEntity.java
+++ comuns/src/main/java/the/coyote/comuns/model/entity/PersonaEntity.java
@@ -1,10 +1,17 @@
 package the.coyote.comuns.model.entity;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import the.coyote.comuns.model.enumeration.TipoCadastro;
 import the.coyote.comuns.model.enumeration.TipoPessoa;
 
 @Getter
@@ -15,8 +22,9 @@ import the.coyote.comuns.model.enumeration.TipoPessoa;
 public class PersonaEntity extends BasicEntity {
 
     private String nomeRazaoSocial;
-    private String apelidaNomeFantazia;
+    private String apelidoNomeFantazia;
     private boolean ativo;
     private TipoPessoa tipoPessoa;
+    private TipoCadastro tipocadastro;
     
 }
